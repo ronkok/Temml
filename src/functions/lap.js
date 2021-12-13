4cb71081e30b8c6e7cbc4e794c05d004b00a05ef
Ron Kok <ronkok@comcast.net>
2021-12-13 14:17:45 -0800

    Work around Firefox bug affecting \mathrlap.

--- src/functions/lap.js
+++ src/functions/lap.js
@@ -5,7 +5,7 @@ import * as mml from "../buildMathML"
 
 defineFunction({
   type: "lap",
-  names: ["\\mathllap", "\\mathrlap", "\\mathclap"],
+  names: ["\\mathllap", "\\mathclap"],
   props: {
     numArgs: 1,
     allowedInText: true
@@ -20,14 +20,37 @@ defineFunction({
     }
   },
   mathmlBuilder: (group, style) => {
-    // mathllap, mathrlap, mathclap
+    // mathllap, mathclap
     const node = new mathMLTree.MathNode("mpadded", [mml.buildGroup(group.body, style)])
-
-    if (group.alignment !== "rlap") {
-      const offset = group.alignment === "llap" ? "-1" : "-0.5"
-      node.setAttribute("lspace", offset + "width")
-    }
+    const offset = group.alignment === "llap" ? "-1" : "-0.5"
+    node.setAttribute("lspace", offset + "width")
     node.setAttribute("width", "0px")
     return node
+  }
+})
+
+defineFunction({
+  type: "mathrlap",
+  names: ["\\mathrlap"],
+  props: {
+    numArgs: 2,
+    allowedInText: true
+  },
+  handler: ({ parser, funcName }, args) => {
+    const body0 = args[0]
+    const body1 = args[1]
+    return {
+      type: "mathrlap",
+      mode: parser.mode,
+      body0,
+      body1
+    }
+  },
+  mathmlBuilder: (group, style) => {
+    const node0 = new mathMLTree.MathNode("mpadded", [mml.buildGroup(group.body0, style)])
+    // The next node also has to be wrapped in <mpadded> or Firefox will not align it properly.
+    const node1 = new mathMLTree.MathNode("mpadded", [mml.buildGroup(group.body1, style)])
+    node0.setAttribute("width", "0px")
+    return new mathMLTree.MathNode("mrow", [node0, node1])
   }
 })
