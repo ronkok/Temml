1789e9df1d9942f3b081e07c3783d7f65fe1a513
Ron Kok <ronkok55@outlook.com>
2023-01-25 17:43:31 -0800

    v0.10.4 release

--- src/functions/ordgroup.js
+++
@@ -1,9 +0,0 @@
-import { defineFunctionBuilders } from "../defineFunction";
-import * as mml from "../buildMathML";
-
-defineFunctionBuilders({
-  type: "ordgroup",
-  mathmlBuilder(group, style) {
-    return mml.buildExpressionRow(group.body, style, true);
-  }
-});
