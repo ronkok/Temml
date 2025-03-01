d4d4ef032a66cba9b0acaa97abe57561991f23ac
Ron Kok <ronkok55@outlook.com>
2025-03-01 09:03:27 -0800

    Copy Fira CSS to dist

--- utils/copyfiles.js
+++ utils/copyfiles.js
@@ -22,6 +22,10 @@ fs.copyFile('site/assets/Temml-Libertinus.css', 'dist/Temml-Libertinus.css', (er
   if (err) { throw err }
 })
 
+fs.copyFile('site/assets/Temml-Fira.css', 'dist/Temml-Fira.css', (err) => {
+  if (err) { throw err }
+})
+
 fs.copyFile('site/assets/Temml-NotoSans.css', 'dist/Temml-NotoSans.css', (err) => {
   if (err) { throw err }
 })
