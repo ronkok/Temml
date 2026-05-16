2d6eb0bcaa7a1b15216493a737b17d792fdbe697
Ron Kok <ronkok55@outlook.com>
2026-05-15 18:47:31 -0700

    Release 0.13.3

--- test/processTests.js
+++
@@ -1,6 +0,0 @@
-const fs = require("fs")  // Node.js file system
-
-let str = fs.readFileSync('./test/katex-spec1.js').toString('utf8')
-str = str.replace(/Expect(`[^`]*`)/g,
-  'Expect($1)')
-fs.writeFileSync('./test/katex-spec2.js', str)
